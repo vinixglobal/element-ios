ac196f38ee4ed730619fd47a34730a618680159f
David Abrego <dbrego@gmail.com>
2020-12-10 11:08:44 -0600

    Changed main tint color of the app

--- Riot/Managers/Theme/Themes/DefaultTheme.swift
+++ Riot/Managers/Theme/Themes/DefaultTheme.swift
@@ -22,7 +22,7 @@ import UIKit
 class DefaultTheme: NSObject, Theme {
 
     var identifier: String = "default"
-    
+
     var backgroundColor: UIColor = UIColor(rgb: 0xFFFFFF)
 
     var baseColor: UIColor = UIColor(rgb: 0xF5F7FA)
@@ -41,12 +41,12 @@ class DefaultTheme: NSObject, Theme {
     var textPrimaryColor: UIColor = UIColor(rgb: 0x171910)
     var textSecondaryColor: UIColor = UIColor(rgb: 0x8F97A3)
 
-    var tintColor: UIColor = UIColor(displayP3Red: 0.05098039216, green: 0.7450980392, blue: 0.5450980392, alpha: 1.0)
+    var tintColor: UIColor = UIColor(rgb: 0xA6009B)
     var tintBackgroundColor: UIColor = UIColor(rgb: 0xe9fff9)
     var tabBarUnselectedItemTintColor: UIColor = UIColor(rgb: 0xC1C6CD)
     var unreadRoomIndentColor: UIColor = UIColor(rgb: 0x2E3648)
-    var lineBreakColor: UIColor = UIColor(rgb: 0xDDE4EE)        
-    
+    var lineBreakColor: UIColor = UIColor(rgb: 0xDDE4EE)
+
     var noticeColor: UIColor = UIColor(rgb: 0xFF4B55)
     var noticeSecondaryColor: UIColor = UIColor(rgb: 0x61708B)
 
@@ -56,7 +56,7 @@ class DefaultTheme: NSObject, Theme {
         UIColor(rgb: 0x03B381),
         UIColor(rgb: 0x368BD6),
         UIColor(rgb: 0xAC3BA8)]
-    
+
     var userNameColors: [UIColor] = [
         UIColor(rgb: 0x368BD6),
         UIColor(rgb: 0xAC3BA8),
@@ -67,7 +67,7 @@ class DefaultTheme: NSObject, Theme {
         UIColor(rgb: 0x5C56F5),
         UIColor(rgb: 0x74D12C)
     ]
-    
+
     var statusBarStyle: UIStatusBarStyle {
         if #available(iOS 13.0, *) {
             return .darkContent
@@ -77,26 +77,26 @@ class DefaultTheme: NSObject, Theme {
     }
     var scrollBarStyle: UIScrollView.IndicatorStyle = .default
     var keyboardAppearance: UIKeyboardAppearance = .light
-    
+
     @available(iOS 12.0, *)
     var userInterfaceStyle: UIUserInterfaceStyle {
         return .light
     }
 
     var placeholderTextColor: UIColor = UIColor(rgb: 0x8F97A3) // Use secondary text color
-    
+
     var selectedBackgroundColor: UIColor = UIColor(rgb: 0xF5F7FA)
-    
+
     var overlayBackgroundColor: UIColor = UIColor(white: 0.7, alpha: 0.5)
     var matrixSearchBackgroundImageTintColor: UIColor = UIColor(rgb: 0xE7E7E7)
-    
+
     func applyStyle(onTabBar tabBar: UITabBar) {
         tabBar.unselectedItemTintColor = self.tabBarUnselectedItemTintColor
         tabBar.tintColor = self.tintColor
         tabBar.barTintColor = self.baseColor
         tabBar.isTranslucent = false
     }
-    
+
     // Note: We are not using UINavigationBarAppearance on iOS 13+ atm because of UINavigationBar directly include UISearchBar on their titleView that cause crop issues with UINavigationController pop.
     func applyStyle(onNavigationBar navigationBar: UINavigationBar) {
         navigationBar.tintColor = self.tintColor
@@ -109,14 +109,14 @@ class DefaultTheme: NSObject, Theme {
         // The navigation bar needs to be opaque so that its background color is the expected one
         navigationBar.isTranslucent = false
     }
-    
+
     func applyStyle(onSearchBar searchBar: UISearchBar) {
         searchBar.searchBarStyle = .default
         searchBar.barTintColor = self.baseColor
         searchBar.isTranslucent = false
         searchBar.backgroundImage = UIImage() // Remove top and bottom shadow
         searchBar.tintColor = self.tintColor
-        
+
         if #available(iOS 13.0, *) {
             searchBar.searchTextField.backgroundColor = self.searchBackgroundColor
             searchBar.searchTextField.textColor = self.searchPlaceholderColor
@@ -126,12 +126,12 @@ class DefaultTheme: NSObject, Theme {
             }
         }
     }
-    
+
     func applyStyle(onTextField texField: UITextField) {
         texField.textColor = self.textPrimaryColor
         texField.tintColor = self.tintColor
     }
-    
+
     func applyStyle(onButton button: UIButton) {
         // NOTE: Tint color does nothing by default on button type `UIButtonType.custom`
         button.tintColor = self.tintColor
